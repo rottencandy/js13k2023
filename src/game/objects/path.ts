172b3c7d9c4897021d5cd02d3f19641e0e2c0c63
Mohammed Saud <md.saud020@gmail.com>
2023-08-24 00:03:31 +0530

    support variable path size

--- src/game/objects/path.ts
+++ src/game/objects/path.ts
@@ -6,6 +6,11 @@ import { Plane } from "./vertices";
 
 let prg: WebGLProgram, vao: WebGLVertexArrayObject, draw: () => void, uniform: any;
 
+let paths = [];
+
+export const setPaths = () => {
+};
+
 CompInit.push((gl) => {
     prg = shaderProgram(gl, vertex, frag);
     [ vao, draw ] = mesh(
@@ -21,6 +26,7 @@ CompRender.push((gl, mat) => {
     bindVAO(gl, vao);
     useProgram(gl, prg);
 
+    uniform('uSize').u1f(2);
     uniform('uPos').u4f(3, 0, 0, 0);
     uniform('uMat').m4fv(mat);
     uniform('uColor').u3f(.5, .4, .4);
--- src/game/objects/path.vert
+++ src/game/objects/path.vert
@@ -3,9 +3,12 @@ precision mediump float;
 
 layout(location=0)in vec4 aPos;
 
+uniform float uSize;
 uniform mat4 uMat;
 uniform vec4 uPos;
 
 void main() {
-    gl_Position = uMat * (uPos + aPos);
+    // scale x axis
+    vec4 atrPos = aPos * vec4(uSize, 1., 1., 1.);
+    gl_Position = uMat * (uPos + atrPos);
 }
